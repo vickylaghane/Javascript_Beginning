a65d603e3f226f413ca39ea66ef16d7ac817b271
vickylaghane <122519133+vickylaghane@users.noreply.github.com>
2025-05-01 19:19:32 +0000

    Changes in container & variables

---
+++ 01_basics/01_variables_nal.js
@@ -0,0 +1,15 @@
+/*
+3 types of variables in JS : 
+    1. var
+    2. let
+    3. const
+
+01. var : old way of declaring the variable
+        : scope of var : can be declared functionally/locally + global scope
+*/
+
+var x = 10;
+function test(){
+    var y = 20
+}
+console.log(x);
